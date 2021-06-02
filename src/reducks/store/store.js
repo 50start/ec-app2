61203b3814e1dd541de6b349a6951a759adcf082
50start <seriebig18@outlook.jp>
2021-06-03 06:33:29 +0900

    06030633

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -7,11 +7,15 @@ import {connectRouter, routerMiddleware} from 'connected-react-router'
 import thunk from 'redux-thunk';
 
 import {UsersReducer} from "../users/reducers";
+import {ProductsReducer} from "../prosucts/reducers";  //reducersを増やしたらimport
 
 export default function createStore(history) {
-  return reduxCreateStore(combineReducers({
-    router: connectRouter(history),
-    users: UsersReducer
+  
+  return reduxCreateStore(
+     combineReducers({
+      products: ProductsReducer,
+      router: connectRouter(history),
+      users: UsersReducer
      }),
      applyMiddleware(
        routerMiddleware(history),
--- src/templates/ProductEdit.jsx
+++ src/templates/ProductEdit.jsx
@@ -1,5 +1,5 @@
 import React,{useCallback, useState} from 'react'
-import {TextInput} from "../components/UIkit";
+import {PrimaryButton, SelectBox, TextInput} from "../components/UIkit";
 
 const ProductEdit = () => {
 
@@ -21,6 +21,18 @@ const ProductEdit = () => {
           setPrice(event.target.value)
          },[setPrice])
 
+         const categories = [
+           {id: "tops", name: "トップス"},
+           {id: "shirts", name: "シャツ"},
+           {id: "pants", name: "パンツ"},
+          ]
+
+          const genders = [
+            {id: "all", name: "すべて"},
+            {id: "male", name: "メンズ"},
+            {id: "female", name: "レディース"},
+           ]
+
    return(
      <section>
        <h2 className="u-text__headline u-text-center">商品の登録・編集</h2>
@@ -33,10 +45,26 @@ const ProductEdit = () => {
               fullWidth={true} label={"商品説明"} multiline={true} required={true}
               onChange={inputDescription} rows={5} value={description} type={"text"}
             />
+
+            <SelectBox
+              label={"カテゴリー"} options={categories} required={true} select={setCategory} value={category}
+            />
+
+            <SelectBox
+              label={"性別"} options={genders} required={true} select={setGender} value={gender}
+            />
+            
             <TextInput
               fullWidth={true} label={"価格"} multiline={false} required={true}
               onChange={inputPrice} rows={1} value={price} type={"number"}
             />
+            <div className="module-spacer--medium"/>
+            <div className="center">
+              <PrimaryButton
+                 label={"商品情報を保存"}
+                 onClick={}
+              />
+            </div>
        </div>
      </section>
      
